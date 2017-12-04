c8bcc21a356a043d065cda124916c14b651b45ec
burningreds <paula.rios.a@gmail.com>
2017-12-04 12:27:57 -0300

    kasdj

--- extractpapers/main.py
+++ extractpapers/main.py
@@ -1,9 +1,31 @@
 from extractpapers.spiders.papers_spider import PapersSpider
 from scrapy.crawler import CrawlerProcess
 
-spider = PapersSpider(url='https://dl.acm.org/citation.cfm?doid=1242572.1242819')
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+sparql = SPARQLWrapper("http://dblp.l3s.de/d2r/sparql")
+sparql.setQuery("""
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+    SELECT DISTINCT ?paper ?homepage
+    WHERE { ?paper foaf:maker ?maker ;
+              foaf:homepage ?homepage .
+            ?maker foaf:name "Aidan Hogan" .
+    }
+    LIMIT 10
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
 process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
-process.crawl(spider, url='https://dl.acm.org/citation.cfm?doid=1242572.1242819')
+
+for result in results["results"]["bindings"]:
+    process.crawl(PapersSpider(url=result["homepage"]["value"]),url=result["homepage"]["value"])
+    print(result["paper"]["value"], result["homepage"]["value"])
+
 process.start()
