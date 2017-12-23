df67d8a58b4937284536ef968a30613b9e2196f2
burningreds <paula.rios.a@gmail.com>
2017-12-23 18:58:42 -0300

    Downloading of papers is complete

---
+++ extractpapers/getlinks/get_links.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import sys
+import re
+import os
+import requests
+from namedentities import *
+
+def get_papers_links(raw_name):
+    user_page = get_user_page(raw_name)
+
+    # Download rdf file
+    r = requests.get(user_page, allow_redirects=True)
+    open(user_page.split("/")[-1], 'wb').write(r.content)
+
+    paper_links = []
+
+    # Get every link to paper rdf files
+    author_file = open(user_page.split("/")[-1], 'r')
+    for line in author_file:
+        split_line = line.split("#authorOf")
+        if len(split_line) > 1:
+            # Format url
+            paper_page = get_paper_page(split_line[1])
+
+            # Download rdf file
+            paper_page_path = paper_page.split("/")[-1]
+            r = requests.get(paper_page, allow_redirects=True)
+            open(paper_page_path, 'wb').write(r.content)
+
+            # Get link to electronic edition of paper
+            paper_file = open(paper_page_path, 'r')
+            for line2 in paper_file:
+                if "#primaryElectronicEdition" in line2:
+                    paper_homepage = line2.split("#primaryElectronicEdition> <")[1]
+                    paper_homepage = paper_homepage.split('>')[0]
+                    paper_links.append(paper_homepage)
+
+            # Close file
+            paper_file.close()
+            os.remove(paper_page_path)
+
+    author_file.close()
+    return paper_links
+
+
+def get_user_page(raw_name):
+    # Formatting name
+    name = raw_name.decode('utf-8')
+    name = ' '.join(word[0].upper() + word[1:] for word in name.split())
+    name = repr(named_entities(name)).replace(';', '=').replace('&', '=').replace("'", "")
+    first, last = name.split(' ')
+    name = last + ":" + first
+    # Getting surname initial
+    initial = name[0].lower()
+    return "http://dblp.org/pers/%s/%s.nt" % (initial, name)
+
+
+def get_paper_page(line):
+    paper_page = line.replace('> <http://dblp.org/rec/', '')
+    paper_page = re.sub(r'[>\s*.]', '', paper_page)
+    paper_page = "http://dblp.uni-trier.de/rec/nt/%s.nt" % paper_page
+    return paper_page
--- extractpapers/main.py
+++ extractpapers/main.py
@@ -2,43 +2,27 @@ from extractpapers.spiders.papers_spider import PapersSpider
 from scrapy.crawler import CrawlerProcess
 from SPARQLWrapper import SPARQLWrapper, JSON
 from fake_useragent import UserAgent
+from getlinks import get_links
 import sys
 
 
 def main(argv):
     name = str(argv)
     print name
-    results = get_papers_links(name)
+    results = get_links.get_papers_links(name)
 
     ua = UserAgent()
-    process = CrawlerProcess({
-            'USER_AGENT': ua.random
-    })
+    #process = CrawlerProcess({
+    #        'USER_AGENT': ua.random
+    #})
 
-    for result in results["results"]["bindings"]:
-        process.crawl(PapersSpider(url=result["homepage"]["value"],name=name),
-                      url=result["homepage"]["value"],
-                      name=name)
-        print(result["paper"]["value"], result["homepage"]["value"])
+    for result in results:
+        #process.crawl(PapersSpider(url=result,name=name),
+        #              url=result,
+        #              name=name)
+        print(result)
 
-    process.start()
-
-
-def get_papers_links(name):
-    sparql = SPARQLWrapper("http://dblp.l3s.de/d2r/sparql")
-    sparql.setQuery("""
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX owl: <http://www.w3.org/2002/07/owl#>
-            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-
-            SELECT DISTINCT ?paper ?homepage
-            WHERE { ?paper foaf:maker ?maker ;
-                      foaf:homepage ?homepage .
-                    ?maker foaf:name '""" + name + """' .
-            }
-        """)
-    sparql.setReturnFormat(JSON)
-    return sparql.query().convert()
+    #process.start()
 
 
 if __name__ == "__main__":
